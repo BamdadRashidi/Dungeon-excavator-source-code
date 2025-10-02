c090bece058c4c14a7f1d223f42ede8f23eda440
Bamdad T-G-L <113779939+BamdadRashidi@users.noreply.github.com>
2025-10-02 11:16:06 -0700

    added the last things essential

---
+++ equipment_manager.gd
@@ -0,0 +1,17 @@
+extends Node
+
+
+
+
+
+var sword_equipped_to_k = false
+var sword_equipped_to_L = false
+
+var lantern_equipped_to_k = false
+var lantern_equipped_to_L = false
+
+var bomb_equipped_to_k = false
+var bomb_equipped_to_L = false
+
+var bow_equipped_to_k = false
+var bow_equipped_to_L = false
